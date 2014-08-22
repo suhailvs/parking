2d46d03264538a41270054c1af2880814225f6cb
suhailvs <suhailvs@gmail.com>
2014-08-22 09:06:15 +0530

    added ajax spinner for login page

--- ajaxviews/views.py
+++ ajaxviews/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib import auth
+import time
 # Create your views here.
 
 def ajax_login(request):
-    #time.sleep(2)   #slowit 
+    time.sleep(2)   #slowit 
     username = request.POST.get('username', '')
     password = request.POST.get('password', '')
     user = auth.authenticate(username=username, password=password)
