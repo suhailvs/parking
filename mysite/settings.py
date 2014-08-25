98c6560ad1883de63b977b709e405825109c7c97
suhailvs <suhailvs@gmail.com>
2014-08-25 23:36:17 +0530

    openshift data dir changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -70,7 +70,7 @@ if 'OPENSHIFT_DATA_DIR' in os.environ:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(DATA_DIR, 'db.sqlite3'),
     }
 }
 
