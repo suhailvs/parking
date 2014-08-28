f5ac889c9178717e67c66a91de7b6f4b47fcaa29
suhailvs <suhailvs@gmail.com>
2014-08-29 00:27:02 +0530

    changed date/time entry format, image preview

--- homepage/models.py
+++ homepage/models.py
@@ -4,16 +4,29 @@ from django import forms
 from django.utils.safestring import mark_safe
 from datetime import date
 
+class MyFileUploadField(forms.ClearableFileInput):
+    def render(self, name, value, attrs=None):
+        html = super(MyFileUploadField, self).render(name, value,attrs)
+        html+= '''<input id="cropcoords" type="hidden" name="cropcoords" value="">
+        <div class="thumbnail" id="previewimage"></div>'''
+        return mark_safe(html);
+        
+class Weeks(models.Model):
+    name=models.CharField(max_length=10)
+    def __unicode__(self):
+        return self.name
+
 class Parking(models.Model):
-    user = models.ForeignKey(User)	
-    pic = models.ImageField("Parking Photos", upload_to="images/")    
-    streetaddress=models.CharField(max_length=200)
+    user = models.ForeignKey(User)    
+    fromtime=models.DateTimeField()
+    totime=models.DateTimeField()
+    days=models.ManyToManyField(Weeks,help_text=None)
+    totalspaces=models.PositiveIntegerField(max_length=3)
+    pic = models.ImageField("Parking Photos", upload_to="images/",blank=True)
     lat=models.CharField(max_length=20)
     lng=models.CharField(max_length=20)
-    totalspaces=models.IntegerField(max_length=3)    
-    fromtime=models.DateTimeField()
-    totime=models.DateTimeField()    
-    upload_date=models.DateTimeField(auto_now_add =True)
+    date_added=models.DateTimeField(auto_now_add =True)
+    streetaddress=models.CharField(max_length=200)
     def is_booked(self):
         od=len(Orders.objects.filter(parking=self,order_date__startswith=date.today()))
         return False if od == 0 else True
@@ -27,8 +40,11 @@ class Orders(models.Model):
     paid=models.BooleanField(default=False)
     
 # Create your models here.
-class ParkingForm(forms.ModelForm):    
+class ParkingForm(forms.ModelForm):   
+    def __init__(self, *args, **kwargs):
+        super(ParkingForm, self).__init__(*args, **kwargs)
+        self.fields['days'].help_text = None 
     class Meta:
         model=Parking
-        exclude=['user','totalspaces','fromtime','totime']        
-        widgets={'lat': forms.HiddenInput(),'lng': forms.HiddenInput()}
+        exclude=['user','fromtime','totime']        
+        widgets={'lat': forms.HiddenInput(),'lng': forms.HiddenInput(),'pic':MyFileUploadField()}
--- homepage/views.py
+++ homepage/views.py
@@ -1,16 +1,37 @@
 from django.shortcuts import render
 from django.views.generic.edit import FormView
-from homepage.models import ParkingForm,Parking
+from homepage.models import ParkingForm,Parking,Orders
 from django.core.urlresolvers import reverse_lazy
 from datetime import datetime
+from PIL import Image as PImage
+import os
+from django.conf import settings
 # Create your views here.
+#from os.path import join as pjoin
+def resize_and_crop(fname,coords):
+    """
+    Resize and crop profile picture.
+    coords=33,44,22,33   ---x:33,y:22,x2:3,y2:4,w:33,h:32    
+    >>> x=[int(i) for i in coords.split(',')]
+    """
+    im = PImage.open(fname)
+    #box = (50, 50, 200, 300) #x1,y1,x2,y2
+    box=[int(i) for i in coords.split(',')]
+    region = im.crop(box)
+    region.save(fname[:-4]+'_crop.jpg',"JPEG")
+    region.thumbnail((160,160), PImage.ANTIALIAS)
+    region.save(fname[:-4]+'_160.jpg', "JPEG")
+    region.thumbnail((48,48), PImage.ANTIALIAS)
+    region.save(fname[:-4]+'_48.jpg', "JPEG")
+    os.remove(fname)
 
 def FindParking(request):
 	return render(request,'guest/find.html',dict(parkings=Parking.objects.all()))
 
 def userpage(request,nm=None):
 	if not nm and request.user.is_active:nm=request.user.username
-	return render(request,'guest/profile.html',{'parkings':Parking.objects.filter(user__username=nm)})
+	return render(request,'guest/profile.html',{'orders':Orders.objects.filter(user=request.user),
+		'parkings':Parking.objects.filter(user__username=nm)})
 class ShareParking(FormView):
 	template_name = 'guest/share.html'
 	form_class = ParkingForm
@@ -19,9 +40,11 @@ class ShareParking(FormView):
 	def form_valid(self, form):
 		# This method is called when valid form data has been POSTed.
 		# It should return an HttpResponse.	
-		form.instance.totalspaces = self.request.POST['totalspaces']
+		#form.instance.totalspaces = self.request.POST['totalspaces']
 		form.instance.fromtime = datetime.strptime(self.request.POST['fromtime'],'%I:%M %p')#11:30 PM
 		form.instance.totime = datetime.strptime(self.request.POST['totime'],'%I:%M %p')
-		form.instance.user = self.request.user
+		form.instance.user = self.request.user		
 		form.save()
+		imfn = os.path.join(settings.MEDIA_ROOT, form.instance.pic.name)	
+		resize_and_crop(imfn,self.request.POST['cropcoords'])
 		return super(ShareParking, self).form_valid(form)
