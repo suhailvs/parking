88cdb462346f5aee0fdb1041ed2ed73470430bac
suhailvs <suhailvs@gmail.com>
2014-09-03 11:16:38 +0530

    added test List Parking

--- ajaxviews/tests.py
+++ ajaxviews/tests.py
@@ -1,15 +1,41 @@
 from django.test import TestCase
-from homepage.models import Parking
+from homepage.models import Parking,Weeks
 from django.contrib.auth.models import User
 
-class AnimalTestCase(TestCase):
-    def setUp(self):
-        User.objects.create_user(username="lorem", email="lorem@test.com",password="s")
-        User.objects.create_user(username="foo", email="foo@test.com",password="s")
+class ListParkingTestCase(TestCase):
+	fixtures = ['somedata.json']
+	def setUp(self):
+		self.user1=User.objects.create_user(username="lorem", email="lorem@test.com",password="s")
+		self.user2=User.objects.create_user(username="foo", email="foo@test.com",password="s")
+        
 
-    def test_animals_can_speak(self):
-        """Animals that can speak are correctly identified"""
-        lion = Animal.objects.get(name="lion")
-        cat = Animal.objects.get(name="cat")
-        self.assertEqual(lion.speak(), 'The lion says "roar"')
-        self.assertEqual(cat.speak(), 'The cat says "meow"')
+	def test_listparking(self):
+		"""Test the ways in which parking can be listed"""
+
+		p1=Parking(user=self.user1,
+        	fromtime=4,
+        	totime=6,
+        	totalspaces=4,        	
+        	lat='34',
+        	lng='43',
+        	description='good parking',
+        	streetaddress='usa')
+		#pic,date_added,status
+		p1.save()
+		# add some weeks:
+		for wk in ['sunday','saturday']:
+			p1.days.add(Weeks.objects.get(name=wk))
+
+		owner = Parking.objects.get(user__username="lorem")
+		self.assertEqual(owner.user.email, 'lorem@test.com')
+		self.assertEqual(owner.user.is_active, True)
+
+	def test_fromTime_toTime(self):
+		"""
+		Test what type of fromtime and totime possible:
+		-----------------------------------------------
+
+			* fromtime > totime which must not be allowed
+
+		"""
+		pass
